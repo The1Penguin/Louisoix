7897bd514ce6b4e3d2aaf9ae3a41d35dba33653f
pingu <nor@acorneroftheweb.com>
2026-04-10 23:17:43 +0200

    No more precompress :D

--- svelte.config.js
+++ svelte.config.js
@@ -5,8 +5,9 @@ const config = {
 	compilerOptions: {
 		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
+
 	},
-	kit: { adapter: adapter() }
+	kit: { adapter: adapter({precompress: false}) }
 };
 
 export default config;
