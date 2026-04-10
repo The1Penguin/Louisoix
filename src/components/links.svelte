2857203168da3bed6b40ab0fc3b5ebcc86ace981
pingu <nor@acorneroftheweb.com>
2026-04-10 23:23:18 +0200

    Center text if it is too big

--- src/components/links.svelte
+++ src/components/links.svelte
@@ -6,7 +6,7 @@
     <p class="font-bold text-xl">{category.name}</p>
     <div class="flex flex-col items-center">
     {#each category.links as link}
-        <a href={link.url} class="hover:text-violet-400 text-lg">{link.name}</a>
+        <a href={link.url} class="text-center hover:text-violet-400 text-lg">{link.name}</a>
     {/each}
     </div>
 </div>
