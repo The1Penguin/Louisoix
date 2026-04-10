75db875d829b96f2d8e0fa91a1f650962d07888b
pingu <nor@acorneroftheweb.com>
2026-04-10 22:29:14 +0200

    Fix overflow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,17 +7,17 @@
 </script>
 
 
-<div class="grid grid-cols-2 items-center justify-center h-screen dark:text-sky-50 ">
-    <div class="flex justify-self-start m-auto">
+<div class="md:grid md:grid-cols-2 md:items-center md:justify-center h-screen dark:text-sky-50 max-md:flex max-md:flex-col-reverse overflow-auto">
+    <div class="flex flex-row max-md:flex-col justify-self-start mx-auto mb-auto md:m-auto">
         {#each data.config.categories as category }
-            <div class="grid-flow-row gap-4 px-4">
+            <div class="my-1">
                 <Links category={category}/>
             </div>
         {/each}
     </div>
-    <div class="flex flex-col items-center justify-self-end m-auto gap-10">
+    <div class="flex flex-col items-center justify-self-end mx-auto mt-5 mb-10 md:gap-10">
         <Clock />
         <p class="text-xl dark:text-color-sky-50">Welcome {data.config.name}</p>
-        <img class="h-68 object-cover inline-block border-solid border-8 border-slate-600 dark:border-sky-100" src="profile.png" alt="Some form of profile pic chosen by the host">
+        <img class="h-68 object-cover inline-block border-solid border-8 border-slate-600 dark:border-sky-100 max-md:hidden" src="profile.png" alt="Some form of profile pic chosen by the host">
     </div>
 </div>
